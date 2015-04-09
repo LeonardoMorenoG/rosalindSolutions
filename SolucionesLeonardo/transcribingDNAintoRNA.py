1181af6c5a200f6f902c7a755cce93b6d5e95bd8
LeonardoMorenoG <lemoga.inv@gmail.com>
2015-04-09 07:39:55 -0500

    beta

---
+++ SolucionesLeonardo/transcribingDNAintoRNA.py
@@ -0,0 +1,35 @@
+"""
+Transcribing DNA into RNA
+@author: lemoga
+Let s a cDNA string and t the transcription of s
+Given: s
+Return: t
+"""
+import sys
+
+def dna2rna(s):
+    """
+    Transcribe cDNA into RNA
+    
+    Parameters:
+    -----------
+    s : string
+        cDNA sequence.
+    
+    Returns:
+    -----------
+    t : string
+        RNA sequence
+    """
+    t = s.replace("T","U")
+    return t 
+
+#Get the sequence
+fr =  open(sys.argv[1],"r")
+sequence = fr.readline()
+fr.close()
+
+#Write the answer
+fw = open(sys.argv[2],"w")
+fw.write(dna2rna(sequence))
+fw.close()
