9893ccf4d60b604a50e4e602d381f0fe4e2539a2
LeonardoMorenoG <lemoga.inv@gmail.com>
2015-08-26 09:00:38 -0500

    template grph exercise

---
+++ SolucionesLeonardo/grph.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+"""
+OverlapGraphsTemplate
+Created on Wed Aug 26 08:41:01 2015
+Given: A collection of DNA strings in FASTA format having total length at most 10 kbp.
+Return: The adjacency list corresponding to O3. You may return edges in any order.
+@author: lemoga
+Usage: python grph.py input.fasta output.txt
+"""
+
+def getSuffixes(sequences, k):
+    """
+    Description
+    
+    Parameters:
+    -----------
+    
+    Returns:
+    -----------
+    
+    """
+
+def getPrefixes(sequences, k):
+    """
+    Description
+    
+    Parameters:
+    -----------
+    
+    Returns:
+    -----------
+    
+    """    
+
+def getMatches(suffixes, prefixes):
+    """
+    Description
+    
+    Parameters:
+    -----------
+    
+    Returns:
+    -----------
+    
+    """
+
+if __name__ == "__main__":
